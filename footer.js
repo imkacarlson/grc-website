abf832d2f53ca213d395575e6d8e17794efcaa75
Keith Carlson <imkacarlson@gmail.com>
2021-06-12 21:10:22 -0400

    Initial commit of v2

---
+++ footer.js
@@ -0,0 +1,7 @@
+// https://www.freeformatter.com/javascript-escape.html#ad-output
+
+document.write('      <footer class=\"footer mt-auto py-3 bg-light\">\r\n        <div class=\"container\">\r\n          <img src=\"\/logos\/f_logo_RGB-Black_58.png\" class=\"img-fluid\" alt=\"...\">\r\n          <img src=\"\/logos\/glyph-logo_May2016.png\" class=\"img-fluid\" alt=\"...\">\r\n          <img src=\"\/logos\/2021 Twitter logo - black.png\" class=\"img-fluid\" alt=\"...\">\r\n        <\/div>\r\n      <\/footer>');
+
+
+// Without root links
+// document.write('    <footer class=\"footer mt-auto py-3 bg-light\">\r\n      <div class=\"container\">\r\n        <img src=\"logos\/f_logo_RGB-Black_58.png\" class=\"img-fluid\" alt=\"...\">\r\n        <img src=\"logos\/glyph-logo_May2016.png\" class=\"img-fluid\" alt=\"...\">\r\n        <img src=\"logos\/2021 Twitter logo - black.png\" class=\"img-fluid\" alt=\"...\">\r\n      <\/div>\r\n    <\/footer>');
